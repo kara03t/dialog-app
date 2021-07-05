f0c58bd54370f72f9bdc4fe2723bcdae3bbbbb83
kara03t <tomoki-kara@hotmail.co.jp>
2021-07-05 16:37:19 +0900

    マイページを少し修正しました

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -2,11 +2,16 @@ class PostsController < ApplicationController
   def create
     p = post_params
     user = User.find(p[:id])
-    if user.posts.create(body: p[:body], submitter: current_user.id)
-      flash[:notice] = 'post was created successfully'
+    if (p[:body]=='')
+      flash[:alert] = 'null comment is not allowed'
     else
-    flash[:alert] = 'something wrong'
+      if user.posts.create(body: p[:body], submitter: current_user.id)
+        flash[:notice] = 'post was created successfully'
+      else
+      flash[:alert] = 'something wrong'
+      end
     end
+    
     redirect_to user_path(user.id)
   end
   protected

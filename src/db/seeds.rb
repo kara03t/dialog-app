6b6113fd17d5bb5e6ae8061ba7bb8095bee474c4
kara03t <tomoki-kara@hotmail.co.jp>
2021-07-11 15:44:31 +0900

    新着投稿の作成

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -4,4 +4,5 @@ names.each_with_index{|name,i|
   uid = sprintf "user%03d", i
   User.create(username: uid, fullname: name,
   password: '123456', role: ((i===0)? 'teacher': 'student'))
+  Visit.create(user_id: 1, visitee: i)
 }
